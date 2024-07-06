b836d139870db71f46c631a8efd0a502f39f3790
Ioan Papuc <papuc_ionut27@yahoo.com>
2024-07-06 20:41:59 +0300

    Added Movie class and MoviesCollection hashmap class to store movies by genres

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,3 @@
-from dataset import genres, collection
-
-
 
 def linear_search(list, target):
     matches = []
@@ -10,4 +7,87 @@ def linear_search(list, target):
     return matches
 
 
-#print("Linear search: \n", linear_search(genres, 'h'))
+
+
+class Movie:
+    def __init__(self, title, genre, year, score = None):
+        self.title = title
+        self.genre = genre
+        self.year = year
+        self.score = score
+    
+    def __repr__(self):
+        ret = " * {0} ({1}), score: {2}".format(self.title, self.year, self.score)
+        return ret
+
+
+
+
+class MoviesCollection:
+    def __init__(self, size):
+        self.array_size = size
+        self.array = [None for i in range(self.array_size)]
+
+    def hash(self, genre, count_collisions = 0):
+        key_bytes = genre.encode()
+        hash_code = sum(key_bytes)
+        return hash_code + count_collisions
+    def compressor(self, hash_code):
+        return hash_code % self.array_size
+    
+    def add_movie(self, movie):
+        genre = movie.genre
+        index = self.compressor(self.hash(genre))
+        loc = self.array[index]
+        if loc is None:
+            self.array[index] = [genre, []]
+            self.array[index][1].append(movie)
+            return
+        if loc[0] == genre:
+            self.array[index][1].append(movie)
+            return
+        count_collisions = 1
+        while loc[0] != genre:
+            new_index = self.compressor(self.hash(genre, count_collisions))
+            new_loc = self.array[new_index]
+            if new_loc is None:
+                self.array[new_index] = [genre, []]
+                self.array[new_index][1].append(movie)
+                return
+            if new_loc[0] == genre:
+                self.array[new_index][1].append(movie)
+                return
+            count_collisions += 1
+        
+    def return_movies_by_genre(self, genre):
+        index = self.compressor(self.hash(genre))
+        loc = self.array[index]
+        if loc is None:
+            return None
+        if loc[0] == genre:
+            return self.array[index][1]
+        count_collisions = 1
+        while loc[0] != genre:
+            new_index = self.compressor(self.hash(genre, count_collisions))
+            new_loc = self.array[new_index]
+            if new_loc is None:
+                return None
+            if new_loc[0] == genre:
+                return self.array[new_index][1]
+            count_collisions += 1
+
+    def print_movies_by_genre(self, genre):
+        print("-------------------------------------------")
+        print("We recommend the following {} movies:\n".format(genre))
+        genre_set = self.return_movies_by_genre(genre)
+        for movie in genre_set:
+            print(movie)
+        print("-------------------------------------------")
+
+
+
+
+
+
+
+
--- recommendation_software.py
+++ recommendation_software.py
@@ -1,22 +1,24 @@
-from dataset import genres, collection
-from algorithms import linear_search
+from dataset import genres_list, collection_list, collection_HashMap
+from algorithms import linear_search, MoviesCollection
 
 
 
 
 def construct_user_choice(genres_list):
     
-    user_input = input("What type of movie would you want to see? Type the beginning of the genre and press enter to check if we have it.\n")
+    user_input = input("What type of movie would you want to see? Type the beginning of the genre and press enter.\n")
     options = linear_search(genres_list, user_input)
 
     if len(options) == 1:
         print("The only option available is: {}.".format(options[0]))
-        answer = input("Would you want to see {} movies? Enter y/n: ".format(options[0]))
-        if answer == 'y':
-            print("You choose " + options[0])
-            return options[0]
-        else:
-            return None
+        while True:
+            answer = input("Would you want to see {} movies? Enter y/n: ".format(options[0]))
+            if answer == 'y':
+                return options[0]
+            elif answer == 'n':
+                return None
+            else:
+                print("Please enter a valid option: 'y' for yes or 'n' for no." )
     elif len(options) > 1:
         print("The options available are: " + ', '.join(options))
         return construct_user_choice(options)
@@ -26,26 +28,27 @@ def construct_user_choice(genres_list):
     
 
 
-
 def print_movies(genres_list, movies_collection):
     genre = construct_user_choice(genres_list)
     if genre:
-        print("Am returnat " + genre)
-        for movie in movies_collection:
-            if movie[0] == genre:
-                print("* {0} ({1})\n IMDB score: {2}".format(movie[1], movie[2], movie[3]))
-    
-    another_genre2 = input("Do you want to choose another type of movies? Enter y/n: ")
-    if another_genre2 =='y':
-        print_movies(genres_list, movies_collection)
-    else:
-        return print("Thank you for using our recommendation software!")
+        movies_collection.print_movies_by_genre(genre)
+
+    while True:
+        another_genre = input("Do you want to choose another type of movies? Enter y/n: ")
+        if another_genre =='y':
+            print_movies(genres_list, movies_collection)
+            return
+        elif another_genre == 'n':
+            print("Thank you for using our recommendation software!")
+            return
+        else:
+            print("Please enter a valid option: 'y' for yes or 'n' for no." )
    
         
 
 
 
-print_movies(genres, collection)
+print_movies(genres_list, collection_HashMap)
 
 
 
